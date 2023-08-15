cb8192f489e82cdec058c556223d2433a5d99edb
Shepherd Xie <shepherd.xie@gmail.com>
2023-08-15 15:26:00 +0800

    init

---
+++ src/main/java/com/orkva/xmall/common/XmallCommonApplication.java
@@ -0,0 +1,13 @@
+package com.orkva.xmall.common;
+
+/**
+ * XmallCommonApplication
+ *
+ * @author Shepherd Xie
+ * @version 2023/8/15
+ */
+public class XmallCommonApplication {
+    public static void main(String[] args) {
+
+    }
+}
